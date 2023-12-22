34897f294e1729881778d0d99b72c83bbeb03854
Ezra <87004138+PicassoEason@users.noreply.github.com>
2023-12-22 13:40:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAlgWEIvBw44j0PoaEZb0dyMkco1lDSWsg",
-    authDomain: "who-animal.firebaseapp.com",
-    databaseURL: "https://who-animal-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "who-animal",
-    storageBucket: "who-animal.appspot.com",
-    messagingSenderId: "1093432018176",
-    appId: "1:1093432018176:web:f436be1abd1eaa37d45fe5",
-    measurementId: "G-DTGVM7LWZE"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+
